e0275ef9b74d4fd627730163682913b0df15aea2
Virvon <80401936+Virvon@users.noreply.github.com>
2022-12-10 21:28:54 +0300

    spawner

--- Scripts/SpawnPoint.cs
+++ Scripts/SpawnPoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Enemy _enemyPrefab;
 
     private Vector2 _position;
 
